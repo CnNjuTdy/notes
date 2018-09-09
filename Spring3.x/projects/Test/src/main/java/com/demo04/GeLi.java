0b5cdd9cb15e54af254ee4cea64dbd432b5054c9
tangdaye <tangdaye1996@hotmail.com>
2018-09-09 22:42:11 +0800

    delete some files

--- Spring3.x/projects/Test/src/main/java/com/demo04/GeLi.java
+++
@@ -1,8 +0,0 @@
-package com.demo04;
-
-/**
- * Created by Tondiyee on 2017/1/24.
- */
-public interface GeLi {
-    public void responseAsk(String response);
-}
--- Spring3.x/projects/Test/src/main/java/com/demo05/GeLi.java
+++
@@ -1,8 +0,0 @@
-package com.demo05;
-
-/**
- * Created by Tondiyee on 2017/1/24.
- */
-public interface GeLi {
-    public void responseAsk(String response);
-}
